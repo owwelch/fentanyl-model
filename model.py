e9edab1ed729ef176c46a087d07deac7cf45adce
owwelch <owelch@oberlin.edu>
2022-02-04 14:26:00 -0500

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,12 @@
-import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
 import pandas as pd
 
 df = pd.read_csv("full_dataset.csv")
+print(df.dtypes)
 
+'''
+model = Sequential()
+model.add(Dense(16, activation = 'relu', input_shape = ()))
+model.add(Dense(10, activation = 'relu'))
+'''
