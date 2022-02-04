3041580f79e089f4036c09bcf0012f8c1593f7d6
Owen Welch <92005935+owwelch@users.noreply.github.com>
2022-02-04 17:33:28 -0500

    Update model.py

--- model.py
+++ model.py
@@ -3,10 +3,18 @@ from keras.layers import Dense
 import pandas as pd
 
 df = pd.read_csv("full_dataset.csv")
-print(df.dtypes)
 
-'''
+
+df = df.drop(columns=['FIPS', 'county_name', 'state_abbr', 'state_name',
+                      'state_FIPS', 'county_FIPS', 'state'])#dropping unnecessary cols
+
+df = df.apply(lambda x: x.astype(float))#converting each column to float
+
+print(df['NFLIS'].head())
+print(df.loc[df['Year'] == 2010].shape)
+
+train_data = 
+
 model = Sequential()
-model.add(Dense(16, activation = 'relu', input_shape = ()))
-model.add(Dense(10, activation = 'relu'))
-'''
+model.add(Dense(16, activation = 'relu', input_shape = (3109, )))
+model.add(Dense(3109, activation = 'relu'))
